e782c93ce152eb8b658b711f2b43a9a3b4e4796b
Andrea Julca <andrea.julca@gmail.com>
2016-10-12 10:48:25 -0400

    Comment example

--- R/bea2List.r
+++ R/bea2List.r
@@ -12,7 +12,7 @@
 #'									'datasetname' = 'NIPA',
 #'									'Frequency' = 'A',
 #'									'TableID' = '68',
-#'									'Year' = 'X')
+#'									'Year' = 'X')		#In this example, 'X' gets all years available
 #' resp <- beaGet(userSpecList, asTable = FALSE)
 #' BL <- bea2List(resp)
 
